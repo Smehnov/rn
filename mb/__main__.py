ac64151d2d86ae3ff98f5952c956899e9831994a
smehnov <alexander.smehnov@gmail.com>
2024-08-01 18:51:17 +0300

    libp2p commands sending

--- mb/__main__.py
+++ mb/__main__.py
@@ -1,11 +1,16 @@
 import typer
 from mb.commands import robots, jobs
-
+from mb.settings import AGENT_SOCKET_PATH
 
 app = typer.Typer()
-app.add_typer(robots.app, name="robot")
-app.add_typer(jobs.app, name="job")
+if AGENT_SOCKET_PATH:
+    app.add_typer(jobs.agent_app, name="jobs")
+else:
+    app.add_typer(robots.app, name="robots")
+    app.add_typer(jobs.app, name="jobs")
 
+def main():
+    app()
 
 if __name__ == "__main__":
-    app()
+    main()
--- mb/commands/robots.py
+++ mb/commands/robots.py
@@ -19,6 +19,10 @@ def list():
         table.add_row(robot['id'], robot['name'], robot['description'], robot['status'], robot['api_key'])
     console.print(table)
 
+@app.command()
+def get(robot_id: str):
+    robot = requests.post(API_URL + '/robots.get', json={'organizationId': ORGANIZATION_ID, 'robotId': robot_id}, headers={'authorization': API_TOKEN}).json()
+    print(robot)
 
 if __name__ == "__main__":
     app()
